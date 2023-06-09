65b7c59a5b167c982407c6e8f35f2d6ec1bbe0a9
Piotrek Rybiec <piotrrybiec28@gmail.com>
2023-06-09 21:13:09 +0200

    primes: is this equivalent?

--- maths/primes.go
+++ maths/primes.go
@@ -54,7 +54,7 @@ func PrimeFactors(n int) (ret []int) {
     n = n / 2
   }
 
-  for i := 3; i*i <= n; i+=2 {
+  for i := 3; i*i <= n; i = i + 2 {
     for n % i == 0 {
       ret = append(ret, i)
       n = n / i 
